883dfd0b4f2e587852b8c2ff969ac4f8b1554c58
tomyolivera <tomasolivera27@gmail.com>
2022-09-14 21:26:28 -0300

    dist

---
+++ dist/assets/Dashboard.8737cf24.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index.d9eb7f30.js";const d=()=>r("div",{children:r("h1",{children:"Dashboard"})});export{d as default};
---
+++ dist/assets/Orders.34bf29e6.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index.d9eb7f30.js";const d=()=>r("div",{children:r("h1",{children:"Orders"})});export{d as default};
